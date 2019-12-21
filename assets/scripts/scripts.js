9538720475ebfbf0a6f45354ed35f474bbb9311d
George Travis <agtravis85@gmail.com>
2019-12-21 12:02:35 -0800

    prev button works

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -2,4 +2,28 @@ var currentDateSpanEl = $('#current-date-span');
 
 var m = moment();
 
-currentDateSpanEl.text(m.toString());
+currentDateSpanEl.text(m.toString()); // change to more user friendly
+
+var prev = $('#prev');
+var next = $('#next');
+var yearInput = selectYear;
+var monthInput = selectMonth;
+var dateInput = selectDate;
+var customDateButton = $('#button-custom-date');
+var periods = $('.period');
+
+var years = m.get('year').toString();
+var months = (m.month() + 1).toString();
+var date = m.date().toString();
+var key = years + months + date;
+
+var momentObject = {};
+
+// setKey();
+// getKey();
+// setColors();
+
+prev.on('click', function() {
+  m.subtract(1, 'days');
+  currentDateSpanEl.text(m.toString());
+});
